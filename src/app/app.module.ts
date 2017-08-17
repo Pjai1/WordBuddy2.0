d4fa4775d27599eb758f7d40bc00182ae1ec1b11
Pjai1 <ulti40@hotmail.com>
2017-08-17 19:51:36 +0200

    Plugins added (finally)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
@@ -10,6 +11,8 @@ import { SettingsPage } from '../pages/settings/settings';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { TextToSpeech } from '@ionic-native/text-to-speech';
+import { SpeechRecognition } from '@ionic-native/speech-recognition';
 
 @NgModule({
   declarations: [
@@ -21,6 +24,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
@@ -34,7 +38,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    TextToSpeech,
+    SpeechRecognition,
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,48 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { SpeechRecognition } from '@ionic-native/speech-recognition';
+import { TextToSpeech } from '@ionic-native/text-to-speech';
+import { Geolocation } from 'ionic-native';
+import { NavController, Platform } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  recognition: any;
+  text: string;
+  speech: Array<string> = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public platform: Platform, public navCtrl: NavController, public http: Http, private speechRecognition: SpeechRecognition, private tts: TextToSpeech) {
 
+  }
+
+  ngOnInit() {
+    this.speechRecognition.isRecognitionAvailable()
+    .then((available: boolean) => console.log(available))
+
+    // Geolocation.getCurrentPosition().then((resp) => {
+    //   alert(resp.coords.latitude)
+    // })
+
+    let options = {
+      text: "hello world",
+      locale: "en-GB",
+      rate: 0.75
+    };
+
+    this.tts.speak(options).then(() => alert('success')).catch((reason: any) => alert(reason));
+  }
+
+  record() {
+    this.speechRecognition.requestPermission()
+    .then(
+      () => alert('Granted'),
+      () => alert('Denied')
+    )
+    this.speechRecognition.startListening().subscribe(data => this.speech = data, error => alert(error))
   }
 
 }
