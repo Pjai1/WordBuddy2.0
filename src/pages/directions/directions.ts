715a3e1883d5ab5155f1c17347b414c94e70d061
Pjai1 <ulti40@hotmail.com>
2017-08-21 20:41:51 +0200

    Added google maps shenanigans finally

--- src/pages/directions/directions.ts
+++ src/pages/directions/directions.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
+declare var google;
 
 
 @Component({
@@ -8,16 +9,90 @@ import { Geolocation } from '@ionic-native/geolocation';
     templateUrl: 'directions.html'
 })
 export class DirectionsPage {
-    location: any;
+    hideRouteSearch: boolean = false;
+    @ViewChild('map') mapElement: ElementRef;
+    map: any;
+    start = 'texas';
+    end = 'chicago';
+    directionsService = new google.maps.DirectionsService;
+    directionsDisplay = new google.maps.DirectionsRenderer;
 
     constructor(public geolocation: Geolocation, public navCtrl: NavController) {
             
     }
 
-    locateMe() {
-        this.geolocation.getCurrentPosition((location) => {
-            console.log(location)
-            this.location = location;
-        }).then().catch((error) => console.log('Error'))
+    ionViewDidLoad() {
+        this.initMap();
     }
+
+    routeSearchWindow() {
+        this.hideRouteSearch = !this.hideRouteSearch;
+    }
+
+    initMap() {
+        this.geolocation.getCurrentPosition().then((location) => {
+            alert(location);
+            this.map = new google.maps.Map(this.mapElement.nativeElement, {
+                    zoom: 7,
+                    center: {lat: location.coords.latitude, lng: location.coords.longitude},
+                    enableHighAccuracy: true,
+                    timeout: 5000,
+                    maximumAge: 0
+                });
+            
+                this.directionsDisplay.setMap(this.map);
+        }).catch((error) => {
+            console.log("Cannot get current position due to " + error);
+            // this worked in browser so had to hardcode to showcase the feature
+            this.map = new google.maps.Map(this.mapElement.nativeElement, {
+                zoom: 7,
+                center: {lat: 51.015827, lng: 4.496858},
+                enableHighAccuracy: true,
+                timeout: 5000,
+                maximumAge: 0
+              });
+          
+              this.directionsDisplay.setMap(this.map);
+        })
+      }
+    
+      calculateAndDisplayRoute() {
+        this.directionsService.route({
+          origin: this.start,
+          destination: this.end,
+          travelMode: 'DRIVING'
+        }, (response, status) => {
+          if (status === 'OK') {
+            this.directionsDisplay.setDirections(response);
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+      }
+
+      addMarker(){
+        
+         let marker = new google.maps.Marker({
+           map: this.map,
+           animation: google.maps.Animation.DROP,
+           position: this.map.getCenter()
+         });
+        
+         let content = "<h4>Your current location</h4>";          
+        
+         this.addInfoWindow(marker, content);
+        
+       }
+
+       addInfoWindow(marker, content){
+        
+         let infoWindow = new google.maps.InfoWindow({
+           content: content
+         });
+        
+         google.maps.event.addListener(marker, 'click', () => {
+           infoWindow.open(this.map, marker);
+         });
+        
+       }
 }
